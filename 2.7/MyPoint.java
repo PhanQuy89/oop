63410fdec33fe7f7f30c1d68989cb1d0427e3b43
PhanQuy89 <quypd.24ite@vku.udn.vn>
2024-11-12 18:22:22 +0700

    bai2.6

---
+++ 2.7/MyPoint.java
@@ -0,0 +1,20 @@
+public class MyPoint {
+    private int x, y;
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+}
